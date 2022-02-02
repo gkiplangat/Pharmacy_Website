b12e3b5fe73f7a1c1dffcf0fa93ae3e888225c73
giddy22-02 <56033401+giddy22-02@users.noreply.github.com>
2022-02-02 15:57:47 +0300

    files config

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="Assets/css/style.css">
+</head>
+<body>
+    <br>
+    <br>
+    <div class="container">
+        <div id="login-form">
+            <h3>User Login</h3>
+            <fieldset>
+                <form action="" id="form-login">
+                    <input type="text" autofocus id="un"placeholder="Username" required autocomplete="off">
+                    <input type="password" autofocus id="up"placeholder="Password" required autocomplete="off">
+                    <input type="submit" name="log"value="Login">
+                </form>
+            </fieldset>
+        </div>
+    </div>
+</body>
+</html>
