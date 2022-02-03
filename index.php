efcd17a8e6fa510fc2ad9540b4b8fce1a32c7c02
giddy22-02 <56033401+giddy22-02@users.noreply.github.com>
2022-02-03 16:05:18 +0300

    style

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="Assets/css/style.css">
+    <link rel="stylesheet" href="Assets/css/style1.css">
 </head>
 
 <body>
