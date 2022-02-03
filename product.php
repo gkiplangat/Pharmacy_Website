7abc6d11e4bd3df316b56850d7274ff4bd8b44eb
giddy22-02 <56033401+giddy22-02@users.noreply.github.com>
2022-02-03 13:48:50 +0300

    product page
    update

---
+++ product.php
@@ -0,0 +1,121 @@
+<?php
+require_once('include/session.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Madawa Pharmacy & Monitoring System</title>
+
+    <!--Bootstrap Core CSS-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-theme.min.css">
+    <!--Custom CSS-->
+    <link rel="stylesheet" href="">
+    <!--Morris Charts CSS-->
+    <link rel="stylesheet" href="">
+    <!--Custom Fonts-->
+    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+    <div id="wrapper">
+        <!-- Navigation-->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display-->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-exl-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="" class="nav-brand" href="index.html">Regis Inventory and Monitoring System</a>
+            </div>
+            <!--Top Menu Items-->
+            <ul class="nav navbar-right top-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>Administrator <b class="caret"></b> </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-gear"></i>Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="logout.php"> <i class="fa fa-fw fa-power-off"></i>Log Out</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <!-- Sidebar Menu Items  -- These Collapse to the responsive navigation menu on Small Screens-->
+            <div class="collapse navbar-collapse navbar-exl-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li class="active">
+                        <a href="home.php"> <span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a>
+                    </li>
+                    <li>
+                        <a href="item.php"> <span class="glyphicon glyphicon-list" aria-hidden="true"></span>Item List</a>
+                    </li>
+                    <li>
+                        <a href="product.php"> <span class="glyphicon glyphicon-th-list" aria-hidden="true"></span>Product Profile</a>
+                    </li>
+                    <li>
+                        <a href="stock.php"> <span class="glyphicon glyphicon-book" aria-hidden="true"></span>Stocks</a>
+                    </li>
+                    <li>
+                        <a href="expired.php"> <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>Expired</a>
+                    </li>
+                    <li>
+                        <a href="sales.php"> <span class="glyphicon glyphicon-record" aria-hidden="true"></span>Sales</a>
+                    </li>
+                </ul>
+            </div>
+            <!--. Navbar - Collapse-->
+        </nav>
+        <div id="page-wrapper">
+            <div class="container-fluid">
+
+                <!-- Page Heading-->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+
+                        </h1>
+                    </div>
+                </div>
+                <!-- /.row-->
+                <button class="btn btn-default" id="add-stock">New Stock
+                    <span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
+                </button>
+
+                <button class="btn btn-danger" id="del-stock">Remove Selected
+                    <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                </button>
+
+                <div id="all-sctocklist"></div>
+            </div>
+            <!--  /.container-fluid-->
+        </div>
+        <!--  /.#page-wrapper-->
+    </div>
+    <!--  /.#wrapper-->
+    <?php include_once('modal/stock.php');?>
+    <?php include_once('modal/confirmation.php');?>
+    <?php include_once('modal/message.php');?>
+
+    <script type="text/javascript" src=""></script>
+    <script type="text/javascript" src=""></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src=""></script>
+    <script type="text/javascript" src=""></script>
+    <script type="text/javascript" src=""></script>
+</body>
+
+</html>
