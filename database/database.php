8bbbfd29cc7fac418751ce0e603018e5d321398d
giddy22-02 <56033401+giddy22-02@users.noreply.github.com>
2022-02-04 12:58:19 +0300

    DATABASE

--- database/database.php
+++ database/database.php
@@ -1,3 +1,55 @@
 <?php
-include_once('connection.php');
+include_once('connection.php'); // My connection is here
+class Database extends Connection{
+    public function __construct(){
+        parent::__construct();//put this is naay constructor and class nai iya ge extendnan para ma apil sad
+        //above code copy the default constructor of the class extended
+        if(session_status() ==PHP_SESSION_NONE)
+        {
+            session_start(); //start session if session not start
+        }
+    }//endDefaultConstructor
+    //disconnect is in the parent class connection.php
+    //get row
+    public function getRow($query,$params =[]){
+        try{
+            $stmt = $this->datab->prepare($query);
+            $stmt->execute($params);
+            return $stmt->fetch();
+        }catch(PDOException $e){
+            throw new Exception($e->getMessage());
+        }
+    }//end getRow
+//get rows
+public function getRows($query,$params = []){
+    try{
+        $stmt = $this->datab->prepare($query);
+        $stmt->execute($params);
+        return $stmt->fetchAll();
+    }catch(PDOException $e){
+        throw new Exception($e->getMessage());
+    }
+}//end getRows
+//insertRow
+public function insertRow($query,$params = []){
+    try{
+        $stmt = $this->datab->prepare($query);
+        $stmt->execute($params);
+        return TRUE;
+    }catch(PDOException $e){
+        throw new Exception($e->getMessage());
+    }
+}//end insertRow
+//Update row
+public function updateRow($query,$params = []){
+    $this->insertRow($query, $params);
+    return true;
+}//end updateRow
+//delete row
+public function deleteRow($query,$params = []){
+    $this->insertRow($query, $params);
+    return true;
+}//end deleteRow
+//get the last inserted ID
+}
 ?>
---
+++ expired.php
@@ -0,0 +1,106 @@
+<?php
+require_once('include/session.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Madawa Pharmacy & Monitoring System</title>
+
+    <!--Bootstrap Core CSS-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet"  type="text/css" href="assets/css/bootstrap-theme.min.css">
+    <!--Custom CSS-->
+    <link rel="stylesheet" href="">
+    <!--Morris Charts CSS-->
+    <link rel="stylesheet" href="">
+    <!--Custom Fonts-->
+    <link rel="stylesheet" type="text/css" href="assets/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+    <div id="wrapper">
+        <!-- Navigation-->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display-->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-exl-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="" class="nav-brand"href="index.html">Regis Inventory and Monitoring System</a>
+            </div>
+            <!--Top Menu Items-->
+            <ul class="nav navbar-right top-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>Administrator <b class="caret"></b> </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-gear"></i>Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="logout.php"> <i class="fa fa-fw fa-power-off"></i>Log Out</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <!-- Sidebar Menu Items  -- These Collapse to the responsive navigation menu on Small Screens-->
+            <div class="collapse navbar-collapse navbar-exl-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li class="active">
+                        <a href="home.php"> <span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a>
+                    </li>
+                    <li>
+                        <a href="item.php"> <span class="glyphicon glyphicon-list" aria-hidden="true"></span>Item List</a>
+                    </li>
+                    <li>
+                        <a href="product.php"> <span class="glyphicon glyphicon-th-list" aria-hidden="true"></span>Product Profile</a>
+                    </li>
+                    <li>
+                        <a href="stock.php"> <span class="glyphicon glyphicon-book" aria-hidden="true"></span>Stocks</a>
+                    </li>
+                    <li>
+                        <a href="expired.php"> <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>Expired</a>
+                    </li>
+                    <li>
+                        <a href="sales.php"> <span class="glyphicon glyphicon-record" aria-hidden="true"></span>Sales</a>
+                    </li>
+                </ul>
+            </div>
+            <!--. Navbar - Collapse-->
+        </nav>
+        <div id="page-wrapper">
+            <div class="conatiner-fluid">
+                <!-- Page Heading-->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                        </h1>
+                    </div>
+                </div>
+                <!-- /.row-->
+                <div id="all-expired"></div>
+            </div>
+            <!-- /.container-fluid-->
+        </div>
+        <!--  /#Page-wrapper-->
+    </div>
+    <!--  /#wrapper-->
+
+    <script type="text/javascript" src=""></script>
+    <script type="text/javascript" src=""></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src=""></script>
+    <script type="text/javascript" src=""></script>
+    <script type="text/javascript" src=""></script>
+</body>
+</html>
