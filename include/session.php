d4de4867098749c5cdad089a30b2d29866da3eb3
giddy22-02 <56033401+giddy22-02@users.noreply.github.com>
2022-02-08 16:23:27 +0300

    session

--- include/session.php
+++ include/session.php
@@ -1,9 +1,10 @@
 <?php
-if(session_status() == PHP_SESSION_NONE)
+/*if(session_status() == PHP_SESSION_NONE)
 {
 	session_start();//start session if session not start
 }
 if(!isset($_SESSION['logged_id'])){
 	die('Access is Denied!');
 }//end isset
+*/
 ?>
